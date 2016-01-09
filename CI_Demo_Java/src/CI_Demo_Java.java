d864c04acf3df4993ecf7efa9d00b0fe60e7544e
gtschmid <gtschmid@googlemail.com>
2016-01-09 13:39:58 +0000

    CIP-3 #Comment Beta release for CI Demo

--- CI_Demo_Java/src/CI_Demo_Java.java
+++ CI_Demo_Java/src/CI_Demo_Java.java
@@ -2,6 +2,6 @@ public class CI_Demo_Java {
 	public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("This is Continuous Integration Test Java Code");
-        System.out.println("Release Alpha");
+        System.out.println("Release Beta");
  }
 }
