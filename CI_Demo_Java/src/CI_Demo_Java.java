4706aa95c27286e839de07dac74d9e651ac48bf0
gtschmid <gtschmid@googlemail.com>
2016-01-09 12:58:38 +0000

    Initial commit 1

---
+++ CI_Demo_Java/src/CI_Demo_Java.java
@@ -0,0 +1,7 @@
+public class CI_Demo_Java {
+	public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("This is Continuous Integration Test Java Code");
+        System.out.println("Git-Jira smart commits. Add Jira issue Key plus comment");
+ }
+}
