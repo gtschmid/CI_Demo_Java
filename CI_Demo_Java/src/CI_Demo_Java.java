6e2ad563b47525d3f44ce20082edd264ee9de5f9
gtschmid <gtschmid@googlemail.com>
2016-01-09 14:27:24 +0000

    CIP-2 #Comment New lines added

--- CI_Demo_Java/src/CI_Demo_Java.java
+++ CI_Demo_Java/src/CI_Demo_Java.java
@@ -1,7 +1,13 @@
 public class CI_Demo_Java {
 	public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
+        System.out.println("");
+        System.out.println("");
         System.out.println("This is Continuous Integration Test Java Code");
+        System.out.println("");
+        System.out.println("");
         System.out.println("Release Charlie");
+        System.out.println("");
+        System.out.println("");
  }
 }
