7f121fed0495f1a214a265b4e4bf15e797a3aefa
gtschmid <gtschmid@googlemail.com>
2016-01-09 13:32:56 +0000

    CIP-1 #Comment First demo release. APLHA version

--- CI_Demo_Java/src/CI_Demo_Java.java
+++ CI_Demo_Java/src/CI_Demo_Java.java
@@ -2,6 +2,6 @@ public class CI_Demo_Java {
 	public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("This is Continuous Integration Test Java Code");
-        System.out.println("Git-Jira smart commits. Add Jira issue Key plus comment");
+        System.out.println("Release Alpha");
  }
 }
