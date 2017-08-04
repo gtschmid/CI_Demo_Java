916d1aee521d11ec3e063870e9e5a947356c3fe8
GE387694 <GE387694@L-156130896.wipro.com>
2017-08-04 23:39:25 +0100

    .

--- CI_Demo_Java/src/CI_Demo_Java.java
+++ CI_Demo_Java/src/CI_Demo_Java.java
@@ -1,15 +1,7 @@
 public class CI_Demo_Java {
 	public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("");
-        System.out.println("");
         System.out.println("This is Continuous Integration");
-        System.out.println("Test Java Code");
-        System.out.println("for RSML");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("Release Arsenal");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("Leading on to Unit Testing");
  }
 }
